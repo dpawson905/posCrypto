9d2bf277cc16bbb2d9f55ce6e96e47c0a41c2a5f
Darrell Pawson <darrellpawson@Darrells-MacBook-Pro.local>
2021-04-18 14:43:21 -0400

    work on POS

--- Node.py
+++ Node.py
@@ -9,13 +9,15 @@ from BlockchainUtils import BlockchainUtils
 
 class Node():
 
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, key=None):
         self.p2p = None
         self.ip = ip
         self.port = port
-        self.blockchain = Blockchain()
         self.transactionPool = TransactionPool()
         self.wallet = Wallet()
+        self.blockchain = Blockchain()
+        if key is not None:
+            self.wallet.fromKey(key)
 
     def startP2P(self):
         self.p2p = SocketCommunication(self.ip, self.port)
@@ -38,3 +40,13 @@ class Node():
             message = Message(self.p2p.socketConnector, 'TRANSACTION', transaction)
             encodedMessage = BlockchainUtils.encode(message)
             self.p2p.broadcast(encodedMessage)
+            forgingRequired = self.transactionPool.forgerRequired()
+            if forgingRequired:
+                self.forge()
+
+    def forge(self):
+        forger = self.blockchain.nextForger()
+        if forger == self.wallet.publicKeyString():
+            print('I am the next forger')
+        else:
+            print('I am not the next forger')
