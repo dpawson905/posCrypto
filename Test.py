db2a2857fd3f195cf1b13ac2ea37f82f83627ee8
Darrell Pawson <darrellpawson@Darrells-MacBook-Pro.local>
2021-04-18 14:02:53 -0400

    work on POS

--- Test.py
+++ Test.py
@@ -1,14 +1,30 @@
 from ProofOfStake import ProofOfStake
 from Lot import Lot
+import string
+import random
 
+def getRandomString(length):
+    letters = string.ascii_lowercase
+    resultString = ''.join(random.choice(letters) for i in range(length))
+    return resultString
+
+bobStake = random.randint(0,100)
+aliceStake = random.randint(0,100)
 
 if __name__ == '__main__':
-    # pos = ProofOfStake()
-    # pos.update('bob', 10)
-    # pos.update('alice', 100)
-    # print(pos.get('bob'))
-    # print(pos.get('alice'))
-    # print(pos.get('jack'))
+    pos = ProofOfStake()
+    pos.update('bob', bobStake)
+    pos.update('alice', aliceStake)
 
-    lot = Lot('bob', 1, 'lastHash')
-    print(lot.lotHash())
+    bobWins = 0
+    aliceWins = 0
+
+    for i in range(100):
+        forger = pos.forger(getRandomString(i))
+        if forger == 'bob':
+            bobWins += 1
+        elif forger == 'alice':
+            aliceWins += 1
+
+    print('bob won: ' + str(bobWins) + ' times with ' + str(bobStake) + ' coins')
+    print('alice won: ' + str(aliceWins) + ' times with ' + str(aliceStake) + ' coins')
