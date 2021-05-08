3e6d0c0cf99c3e89aa434d0ce41dceb5a5c391aa
Darrell Pawson <darrellpawson@Darrells-MacBook-Pro.local>
2021-05-08 15:35:10 -0400

    wrap up and adding more testing

--- Interaction.py
+++ Interaction.py
@@ -2,16 +2,27 @@ from Wallet import Wallet
 from BlockchainUtils import BlockchainUtils
 import requests
 
-if __name__ == '__main__':
 
-    bob = Wallet()
-    alice = Wallet()
-    exchange = Wallet()
-
-    transaction = exchange.createTransaction(
-        alice.publicKeyString(), 10, 'EXCHANGE')
-
+def postTransaction(sender, receiver, amount, type):
+    transaction = sender.createTransaction(
+        receiver.publicKeyString(), amount, type)
     url = "http://localhost:5000/transaction"
     package = {'transaction': BlockchainUtils.encode(transaction)}
     request = requests.post(url, json=package)
     print(request.text)
+
+if __name__ == '__main__':
+
+    bob = Wallet()
+    alice = Wallet()
+    alice.fromKey('keys/stakerPrivateKey.pem')
+    exchange = Wallet()
+
+    # forger: genesis
+    postTransaction(exchange, alice, 100, 'EXCHANGE')
+    postTransaction(exchange, bob, 100, 'EXCHANGE')
+    postTransaction(alice, alice, 25, 'STAKE')
+
+    # forger: alice
+    postTransaction(alice, bob, 1, 'TRANSFER')
+    
